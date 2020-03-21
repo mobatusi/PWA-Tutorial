ef72d65cb54c39481fa39b25585d34b5a615dc7f
Dolu Obatusin <mobatusin.tcsec@gmail.com>
2020-03-21 10:29:10 -0400

    implement sending POST requests via fetch

--- course-project-starting-setup/public/src/js/app.js
+++ course-project-starting-setup/public/src/js/app.js
@@ -38,6 +38,27 @@ fetch('https://httpbin.org/ip')
     })
     ;
 
+
+fetch('https://httpbin.org/post', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    mode: 'cors',
+    body: JSON.stringify({message: 'Does this work?'})
+})
+    .then(function(response) {
+    console.log(response);
+    return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+    ;
 //promise.then(function(text){
 //    return text;
 //}, function(err) {
