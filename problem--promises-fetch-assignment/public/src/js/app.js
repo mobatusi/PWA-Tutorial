678a25a26e6425c5ee34c8dcc35aa944e98d2942
Dolu Obatusin <mobatusin.tcsec@gmail.com>
2020-03-21 12:27:01 -0400

    add soluton to problem

--- problem--promises-fetch-assignment/public/src/js/app.js
+++ problem--promises-fetch-assignment/public/src/js/app.js
@@ -4,26 +4,67 @@ var output = document.querySelector('#output');
 
 button.addEventListener('click', function() {
   // Create a new Promise here and use setTimeout inside the function you pass to the constructor
+  //  setTimeout(function() { // <- Store this INSIDE the Promise you created!
+  //    // Resolve the following URL: https://swapi.co/api/people/1
+  //  }, 3000);
 
-  setTimeout(function() { // <- Store this INSIDE the Promise you created!
-    // Resolve the following URL: https://swapi.co/api/people/1
-  }, 3000);
+    // Handle the Promise "response" (=> the value you resolved) and return a fetch()
+    // call to the value (= URL) you resolved (use a GET request)
 
-  // Handle the Promise "response" (=> the value you resolved) and return a fetch()
-  // call to the value (= URL) you resolved (use a GET request)
+    // Handle the response of the fetch() call and extract the JSON data, return that
+    // and handle it in yet another then() block
 
-  // Handle the response of the fetch() call and extract the JSON data, return that
-  // and handle it in yet another then() block
+    // Finally, output the "name" property of the data you got back (e.g. data.name) inside
+    // the "output" element (see variables at top of the file)
 
-  // Finally, output the "name" property of the data you got back (e.g. data.name) inside
-  // the "output" element (see variables at top of the file)
+    // Repeat the exercise with a PUT request you send to https://httpbin.org/put
+    // Make sure to set the appropriate headers (as shown in the lecture)
+    // Send any data of your choice, make sure to access it correctly when outputting it
+    // Example: If you send {person: {name: 'Max', age: 28}}, you access data.json.person.name
+    // to output the name (assuming your parsed JSON is stored in "data")
 
-  // Repeat the exercise with a PUT request you send to https://httpbin.org/put
-  // Make sure to set the appropriate headers (as shown in the lecture)
-  // Send any data of your choice, make sure to access it correctly when outputting it
-  // Example: If you send {person: {name: 'Max', age: 28}}, you access data.json.person.name
-  // to output the name (assuming your parsed JSON is stored in "data")
+//var promise = new Promise(function(resolve, reject) {
+//    setTimeout(function() {
+//        resolve("https://swapi.co/api/people/1");
+//        }, 3000);
+//})
+//    .then(function(url){
+//        return fetch(url);
+//    })
+//    .then(function(response){
+//        return response.json();
+//    })
+//    .then(function(data){
+//        output.textContent = data.name;
+//    });
+
+
+var promise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve("https://httpbin.org/put");
+        }, 3000);
+})
+    .then(function(url){
+        return fetch(url,{
+        method: 'PUT',
+             headers: {
+                 'Content-Type': 'application/json',
+                 'Accept': 'application/json'
+             },
+             body: JSON.stringify({person: {name: 'Dolu', age: 30}})
+        });
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        output.textContent= data.json.person.name;
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
 
   // To finish the assignment, add an error to URL and add handle the error both as
   // a second argument to then() as well as via the alternative taught in the module
+
 });
