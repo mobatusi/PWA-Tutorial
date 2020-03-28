d2de3e495ad376f7d236862c1070cad2fda042a8
Dolu Obatusin <mobatusin.tcsec@gmail.com>
2020-03-28 08:59:16 -0400

    add implementation of cache strategies and routing

--- course-project-starting-setup/public/sw.js
+++ course-project-starting-setup/public/sw.js
@@ -1,14 +1,7 @@
 
-var CACHE_STATIC_NAME = 'static-v11';
+var CACHE_STATIC_NAME = 'static-v12';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
-
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
-  event.waitUntil(
-    caches.open(CACHE_STATIC_NAME)
-      .then(function(cache) {
-        console.log('[Service Worker] Precaching App Shell');
-        cache.addAll([
+var STATIC_FILES =[
           '/',
           '/index.html',
           '/offline.html',
@@ -24,7 +17,14 @@ self.addEventListener('install', function(event) {
           'https://fonts.googleapis.com/css?family=Roboto:400,700',
           'https://fonts.googleapis.com/icon?family=Material+Icons',
           'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-        ]);
+]
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME)
+      .then(function(cache) {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.addAll(STATIC_FILES);
       })
   )
 });
@@ -46,27 +46,99 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        } else {
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());
-                  return res;
-                })
+    var url = 'https://httpbin.org/get';
+    if(event.request.url.indexOf(url) > -1) {
+        event.respondWith(
+            caches.open(CACHE_DYNAMIC_NAME)
+            .then(function(cache) {
+                return fetch(event.request)
+                    .then(function(res) {
+                     cache.put(event.request, res.clone());
+                     return res;
+                    });
             })
-            .catch(function(err) {
-                return caches.open(CACHE_STATIC_NAME)
-                .then(function(cache) {
-                    return cache.match('/offline.html');
+        );
+    }
+    else if( new RegExp('\\b' + STATIC_FILES.join('\\b')).test(event.request.url)){
+      event.respondWith(
+        fetch(event.request)
+    );
+    }
+    else{
+     event.respondWith(
+        caches.match(event.request)
+          .then(function(response) {
+            if (response) {
+              return response;
+            } else {
+              return fetch(event.request)
+                .then(function(res) {
+                  return caches.open(CACHE_DYNAMIC_NAME)
+                    .then(function(cache) {
+                      cache.put(event.request.url, res.clone());
+                      return res;
+                    })
+                })
+                .catch(function(err) {
+                    return caches.open(CACHE_STATIC_NAME)
+                    .then(function(cache) {
+                        if(event.request.url.indexOf('/help')){
+                            return cache.match('/offline.html');
+                        }
+                    });
                 });
-            });
-        }
-      })
+            }
+          })
+      );
+    }
+
+});
+
+//self.addEventListener('fetch', function(event) {
+//  event.respondWith(
+//    caches.match(event.request)
+//      .then(function(response) {
+//        if (response) {
+//          return response;
+//        } else {
+//          return fetch(event.request)
+//            .then(function(res) {
+//              return caches.open(CACHE_DYNAMIC_NAME)
+//                .then(function(cache) {
+//                  cache.put(event.request.url, res.clone());
+//                  return res;
+//                })
+//            })
+//            .catch(function(err) {
+//                return caches.open(CACHE_STATIC_NAME)
+//                .then(function(cache) {
+//                    return cache.match('/offline.html');
+//                });
+//            });
+//        }
+//      })
+//  );
+//});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request)
+        .catch(function(err) {
+            return caches.match(event.requests);
+        })
   );
 });
+
+// Cache-Only
+//self.addEventListener('fetch', function(event) {
+//  event.respondWith(
+//    fetch(event.request)
+//    );
+//  });
+
+//  Network-only
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request)
+    );
+  });
